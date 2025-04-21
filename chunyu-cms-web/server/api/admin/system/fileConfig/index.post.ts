2b53bceb25a7365aeb87d1e86e93a21d7e467ed6
yinchunyu <542968439@qq.com>
2025-04-21 14:25:24 +0800

    文件配置接口

---
+++ chunyu-cms-web/server/api/admin/system/fileConfig/index.post.ts
@@ -0,0 +1,9 @@
+import { FileConfigServices } from '~/server/services/admin/system/fileConfig/fileConfig.services';
+
+const fileConfigServices = new FileConfigServices();
+
+export default defineEventHandler(async event => {
+  const body = await readBody(event);
+  const data = await fileConfigServices.add(body);
+  return createApiResponse(data);
+});
---
+++ chunyu-cms-web/server/api/admin/system/fileConfig/list.get.ts
@@ -0,0 +1,8 @@
+import { FileConfigServices } from '~/server/services/admin/system/fileConfig/fileConfig.services';
+
+const fileConfigServices = new FileConfigServices();
+
+export default defineEventHandler(async () => {
+  const data = await fileConfigServices.pageList();
+  return createApiResponse(data);
+});
