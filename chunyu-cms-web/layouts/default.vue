d1caa64dd2f3847cff0d8252b6b66770c0a0332b
yinchunyu <542968439@qq.com>
2025-04-23 22:42:13 +0800

    调整样式

--- chunyu-cms-web/layouts/default.vue
+++ chunyu-cms-web/layouts/default.vue
@@ -2,10 +2,10 @@
   <div>
     <div
       class="fixed top-0 left-0 h-100vh transition-all duration-300 overflow-hidden z-999 lt-md:hidden"
-      :class="sidebarOpen ? 'w-220px' : 'w-64px'"
+      :class="sidebarOpen ? 'w-180px' : 'w-64px'"
     >
       <nuxt-link to="/" class="flex justify-center items-center gap-x-5px h-74px bg-#111214">
-        <img src="../assets/images/logo.png" alt="" class="h-34px border-rd-5px" />
+        <img src="../assets/images/logo.png" alt="" class="h-28px border-rd-5px" />
         <span v-if="textVisible" class="color-#fff text-24px font-bold logo-text">淳渔影视</span>
       </nuxt-link>
       <ul class="sidebar-menu-inner">
@@ -37,24 +37,35 @@
         <p class="mt-15px">Copyright 2025 淳渔影视网 Inc. All Rights Reserved.</p>
       </div>
     </div>
-    <div class="main-content transition-all duration-300 lt-md:ml-0px" :class="sidebarOpen ? 'ml-220px' : 'ml-64px'">
-      <div class="flex w-full bg-[rgba(17,17,20,0.8)] items-center gap-10px h-54px pl-16px fixed z-10 lt-md:hidden">
-        <el-icon size="26" color="#f2f2f2" class="cursor-pointer" @click="handleSetSideBar">
-          <ElIconFold v-if="sidebarOpen" />
-          <ElIconExpand v-else />
-        </el-icon>
-        <div class="search-input">
-          <input
-            v-model="keyword"
-            type="text"
-            placeholder="请输入关键字"
-            style="outline: 0"
-            autocomplete="off"
-            @keyup.enter="router.push(`/search?keyword=${keyword}`)"
-          />
-          <nuxt-link :to="`/search?keyword=${keyword}`" class="search-button">
-            <el-icon><ElIconSearch /></el-icon>
-          </nuxt-link>
+    <div class="main-content transition-all duration-300 lt-md:ml-0px" :class="sidebarOpen ? 'ml-180px' : 'ml-64px'">
+      <div
+        :class="sidebarOpen ? 'pl-196px' : 'pl-80px'"
+        class="flex w-full bg-[rgba(17,17,20,0.8)] items-center justify-between gap-10px h-54px fixed left-0 pr-15px z-10 transition-all duration-300 lt-md:hidden"
+      >
+        <div class="flex items-center gap-10px">
+          <el-icon size="26" color="#f2f2f2" class="cursor-pointer" @click="handleSetSideBar">
+            <ElIconFold v-if="sidebarOpen" />
+            <ElIconExpand v-else />
+          </el-icon>
+          <div class="search-input">
+            <input
+              v-model="keyword"
+              type="text"
+              placeholder="请输入关键字"
+              style="outline: 0"
+              autocomplete="off"
+              @keyup.enter="router.push(`/search?keyword=${keyword}`)"
+            />
+            <nuxt-link :to="`/search?keyword=${keyword}`" class="search-button">
+              <el-icon><ElIconSearch /></el-icon>
+            </nuxt-link>
+          </div>
+        </div>
+        <div
+          class="w-88px h-34px bg-#ffffff40 border-rd-8px flex justify-center items-center gap-5px cursor-pointer select-none"
+        >
+          <ElIconUserFilled class="w-18px"></ElIconUserFilled>
+          登录
         </div>
       </div>
       <div class="flex justify-between h-58px items-center p-x-20px md:hidden fixed w-full bg-#111214 z-10">
@@ -62,19 +73,6 @@
           <img src="../assets/images/logo.png" alt="" class="h-28px border-rd-5px" />
           <span class="color-#fff text-20px font-bold logo-text">淳渔影视</span>
         </nuxt-link>
-        <div class="search-input">
-          <input
-            v-model="keyword"
-            type="text"
-            placeholder="请输入关键字"
-            style="outline: 0; width: 160px"
-            autocomplete="off"
-            @keyup.enter="router.push(`/search?keyword=${keyword}`)"
-          />
-          <nuxt-link :to="`/search?keyword=${keyword}`" class="search-button">
-            <el-icon><ElIconSearch /></el-icon>
-          </nuxt-link>
-        </div>
         <el-icon size="26" color="#f2f2f2" class="cursor-pointer" @click="sidebarMobileOpen = true">
           <ElIconFold v-if="sidebarMobileOpen" />
           <ElIconExpand v-else />
@@ -82,6 +80,13 @@
       </div>
       <div class="h-58px md:hidden"></div>
       <slot />
+      <div class="text-10px color-[rgba(255,255,255,0.6)] p-x-20px mb-15px lg-md:hidden">
+        本网站为淳渔CMS演示站，提供的电视剧和电影资源均系收集于各大视频网站
+        若本站收录的节目无意侵犯了贵司版权,请给542968439@qq.com留言,我们会及时逐步删除和规避程序自动搜索采集到的不提供分享的版权影视。
+        本站仅供测试和学习交流。请大家支持正版。
+
+        <p class="mt-15px">Copyright 2025 淳渔影视网 Inc. All Rights Reserved.</p>
+      </div>
     </div>
 
     <el-drawer
@@ -112,12 +117,25 @@
           </nuxt-link>
         </li>
       </ul>
-      <div class="text-10px color-[rgba(255,255,255,0.6)] p-x-20px absolute bottom-10">
-        本网站为淳渔CMS演示站，提供的电视剧和电影资源均系收集于各大视频网站
-        若本站收录的节目无意侵犯了贵司版权,请给542968439@qq.com留言,我们会及时逐步删除和规避程序自动搜索采集到的不提供分享的版权影视。
-        本站仅供测试和学习交流。请大家支持正版。
+      <div class="search-input mt-10px">
+        <input
+          v-model="keyword"
+          type="text"
+          placeholder="请输入关键字"
+          style="outline: 0; width: 100%"
+          autocomplete="off"
+          @keyup.enter="router.push(`/search?keyword=${keyword}`)"
+        />
+        <nuxt-link :to="`/search?keyword=${keyword}`" class="search-button">
+          <el-icon><ElIconSearch /></el-icon>
+        </nuxt-link>
+      </div>
 
-        <p class="mt-15px">Copyright 2025 淳渔影视网 Inc. All Rights Reserved.</p>
+      <div
+        class="absolute bottom-20px right-20px w-88px h-34px bg-#ffffff40 border-rd-8px flex justify-center items-center gap-5px cursor-pointer select-none"
+      >
+        <ElIconUserFilled class="w-18px"></ElIconUserFilled>
+        登录
       </div>
     </el-drawer>
   </div>
@@ -161,7 +179,7 @@
       a {
         @apply p-x-12px p-y-10px flex items-center gap-x-10px m-x-8px m-y-2px;
         span {
-          @apply color-#fff;
+          @apply color-[rgba(255,255,255,0.8)];
         }
         &:hover {
           background: #2d2f39;
@@ -188,30 +206,28 @@
       }
     }
   }
-  .main-content {
-    .search-input {
-      position: relative;
-      input {
-        color: #fff;
-        border: none;
-        background-color: rgba(0, 0, 0, 0.2) !important;
-        outline: none;
-        border-radius: 50px;
-        padding: 6px 20px;
-        -webkit-backdrop-filter: blur(5px);
-        backdrop-filter: blur(5px);
-        width: 400px;
-        font-size: 14px;
-        box-shadow: 0 5px 35px rgba(0, 0, 0, 0.2);
-        border-top: 1px solid rgba(255, 255, 255, 0.1);
-        border-bottom: 1px solid rgba(255, 255, 255, 0.1);
-      }
-      .search-button {
-        position: absolute;
-        right: 15px;
-        top: 8px;
-        cursor: pointer;
-      }
+  .search-input {
+    position: relative;
+    input {
+      color: #fff;
+      border: none;
+      background-color: rgba(0, 0, 0, 0.2) !important;
+      outline: none;
+      border-radius: 50px;
+      padding: 6px 20px;
+      -webkit-backdrop-filter: blur(5px);
+      backdrop-filter: blur(5px);
+      width: 400px;
+      font-size: 14px;
+      box-shadow: 0 5px 35px rgba(0, 0, 0, 0.2);
+      border-top: 1px solid rgba(255, 255, 255, 0.1);
+      border-bottom: 1px solid rgba(255, 255, 255, 0.1);
+    }
+    .search-button {
+      position: absolute;
+      right: 15px;
+      top: 8px;
+      cursor: pointer;
     }
   }
   .logo-text {
