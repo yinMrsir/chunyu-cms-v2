9e4a285b2c02c6ead85ffec4cccab0d78944ca16
yinchunyu <542968439@qq.com>
2025-05-08 17:28:43 +0800

    用户端上传短视频添加分片上传

--- chunyu-cms-web/server/api/admin/common/mergeChunks.post.ts
+++ chunyu-cms-web/server/api/admin/common/mergeChunks.post.ts
@@ -2,20 +2,26 @@ import { createWriteStream, promises as fs, readFileSync, statSync } from 'fs';
 import { join } from 'path';
 import dayjs from 'dayjs';
 import fsExtra from 'fs-extra';
+import { SharedServices } from '~/server/services/admin/share/shared.services';
 
 const { readdir, unlink, rm } = fs;
 const resolve = (p: string) => join(process.cwd(), p);
 const ensureDir = fsExtra.ensureDir;
 const { uploadPath, imgHost } = useRuntimeConfig();
+const shareServices = new SharedServices();
 
 export default defineEventHandler(async event => {
   const { fileId, fileName } = await readBody(event);
+
+  const index = fileName.lastIndexOf('.');
+  const mimeType = fileName.substring(index + 1);
 
   const chunkDir = resolve(`./uploads/chunks/${fileId}`);
 
   const currentDate = dayjs().format('YYYY-MM-DD');
   const dir = resolve(`${uploadPath}/${currentDate}`);
-  const targetPath = `${dir}/${fileName}`;
+  const rename = `${shareServices.generateRandomValue(9)}.${mimeType}`;
+  const targetPath = `${dir}/${rename}`;
 
   await ensureDir(dir);
 
@@ -35,11 +41,10 @@ export default defineEventHandler(async event => {
   await rm(chunkDir, { recursive: true });
 
   const size = statSync(targetPath).size;
-  const index = fileName.lastIndexOf('.');
-  const mimeType = fileName.substring(index + 1);
+
   return createApiResponse({
     name: fileName,
-    url: imgHost + `${uploadPath}/${currentDate}/${fileName}`,
+    url: imgHost + `${uploadPath}/${currentDate}/${rename}`,
     mimeType,
     size,
     path: targetPath
